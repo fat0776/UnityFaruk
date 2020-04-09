9922cd068ec41dbc2762fe94c74cc8793dfa40c8
fat0776 <63000005+fat0776@users.noreply.github.com>
2020-04-09 17:22:24 +0200

    V1 Projekt mit Männchen&Hintergrund
    Im Projekt ist das Männchen vom letzten Treffen enthalten. Zusätzlich habe ich noch ein beispielhaftes Foto als Hindergrund ausgewählt.
    -> Die Animation könnt ihr nochmal anpassen; kriegt man noch bestimmt besser hin

---
+++ GitUnityProjekt/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed = 5f;
+    Vector2 movement;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("screw"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
