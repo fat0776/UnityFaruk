35fc9a888cb3846f4906328347ba7679b56cb755
fat0776 <63000005+fat0776@users.noreply.github.com>
2020-04-17 13:04:55 +0200

    FarukScene Collider hinzugefügt + Kamera angepasst
    Neues Kamera Skript hinzugefügt -> Kamera Ansicht kann limitiert/beschränkt werden
    Grenzen für die Beschränkung müssen in jeder Szene individuell angepasst werden -> Nähere Infos im nächsten Meeting

---
+++ GitUnityProjekt/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+
+    [SerializeField]
+    float timeOffset;
+
+    [SerializeField]
+    Vector2 posOffset;
+
+    [SerializeField]
+    float leftLimit;
+    [SerializeField]
+    float rightLimit;
+    [SerializeField]
+    float bottomLimit;
+    [SerializeField]
+    float topLimit;
+
+    private Vector3 velocity;
+
+    private void FixedUpdate()
+    {
+        //Cameras current position
+        Vector3 startPos = transform.position;
+        //Players current position
+        Vector3 endPos = player.transform.position;
+
+        endPos.x += posOffset.x;
+        endPos.y += posOffset.y;
+        endPos.z = -10;
+
+        transform.position = Vector3.SmoothDamp(startPos, endPos, ref velocity, timeOffset * Time.deltaTime);
+
+        //Camera view limit
+        transform.position = new Vector3
+            (
+            Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
+            Mathf.Clamp(transform.position.y, bottomLimit, topLimit),
+                        transform.position.z
+            );
+    }
+    //Camera view limit visualisieren mit rotem Rechteck
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(new Vector2(leftLimit, topLimit), new Vector2(rightLimit, topLimit));
+        Gizmos.DrawLine(new Vector2(rightLimit, topLimit), new Vector2(rightLimit, bottomLimit));
+        Gizmos.DrawLine(new Vector2(rightLimit, topLimit), new Vector2(leftLimit, bottomLimit));
+        Gizmos.DrawLine(new Vector2(leftLimit, bottomLimit), new Vector2(leftLimit, topLimit));
+
+
+    }
+
+}
