808ee47ec09c266aa39a36a6a20f848993f1c9b6
Smofe96 <63002364+Smofe96@users.noreply.github.com>
2020-04-19 17:00:09 +0200

    Teleporter hinzugefügt
    rechte grüne Lego Plattform führt automatisch zur SceneVorlage (beispielhaft)

---
+++ GitUnityProjekt/Assets/Scripts/TeleportScripts/LevelController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour { 
+
+    public int index;
+    public string levelName;
+    void OnTriggerEnter2D(Collider2D other)
+
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(index);
+            SceneManager.LoadScene(levelName);
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
