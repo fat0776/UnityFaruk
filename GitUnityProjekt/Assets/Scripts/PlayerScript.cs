dc757db65f229f177b21a3230193575209e687c1
fat0776 <63000005+fat0776@users.noreply.github.com>
2020-04-22 18:05:15 +0200

    Bug behoben

--- GitUnityProjekt/Assets/Scripts/PlayerScript.cs
+++ GitUnityProjekt/Assets/Scripts/PlayerScript.cs
@@ -20,15 +20,21 @@ public class PlayerScript : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if ((Input.GetAxis("Vertical") > 0) && (Input.GetAxis("Horizontal")>0) || (Input.GetAxis("Vertical")< 0) && (Input.GetAxis("Horizontal")<0) || (Input.GetAxis("Vertical")< 0) && (Input.GetAxis("Horizontal")>0) || (Input.GetAxis("Vertical")> 0) && (Input.GetAxis("Horizontal")<0))
+        {
+            rb.MovePosition(rb.position + movement * moveSpeed * 0.7f * Time.fixedDeltaTime);
+        } else {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        }
 
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    /*private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("screw"))
         {
             Destroy(other.gameObject);
         }
     }
+    */
 }
