36467887aa193f0bffb7c0fbdcfaca1e7f0fdc24
Smofe96 <63002364+Smofe96@users.noreply.github.com>
2020-04-23 16:59:26 +0200

    Level Update - SaveAndReturnToPosition

---
+++ GitUnityProjekt/Assets/Scripts/TeleportScripts/LevelControllerCharacter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelControllerCharacter : MonoBehaviour
+{
+
+    public static Dictionary<int, Vector2>
+        savedPositions =
+        new Dictionary<int, Vector2>();
+
+    public bool ready = false;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (!ready)
+        {
+            UnityEngine.Debug.Log("Start");
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            UnityEngine.Debug.Log("Start on: " + sceneIndex);
+
+            if (savedPositions.ContainsKey(sceneIndex))
+            {
+                UnityEngine.Debug.Log("Got it");
+                Vector2 position = savedPositions[sceneIndex];
+                UnityEngine.Debug.Log("Load: " + sceneIndex + " - " + transform.position);
+                transform.position = position;
+            }
+            ready = true;
+        }
+    }
+
+    void OnDestroy()
+    {
+        UnityEngine.Debug.Log("onDestroy");
+
+        int sceneIndex =
+            SceneManager
+                .GetActiveScene()
+                .buildIndex;
+        UnityEngine.Debug.Log(sceneIndex);
+
+        UnityEngine.Debug.Log("Save: " + sceneIndex + " - " + transform.position);
+        savedPositions[sceneIndex] = transform.position;
+    }
+
+}
--- GitUnityProjekt/Assets/Scripts/TeleportScripts/Teleporter.cs
+++ GitUnityProjekt/Assets/Scripts/TeleportScripts/Teleporter.cs
@@ -1,33 +1,30 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LevelController : MonoBehaviour {
+public class Teleporter : MonoBehaviour { 
 
-    static int sec = 1;
+    
     public string levelName;
     void OnTriggerEnter2D(Collider2D other)
-    {
-        if ((DateTime.Now.Ticks - start) < sec*100000000) return;
-        UnityEngine.Debug.Log(start);
 
+    {
         if (other.CompareTag("Player"))
-        {
+        { 
             SceneManager.LoadScene(levelName);
-            start = DateTime.Now.Ticks;
         }
     }
-
-    public static long start = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        
     }
 }
