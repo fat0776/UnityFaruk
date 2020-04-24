9e773901c0beff94d294d93f50a0f17ea2f75a5a
Smofe96 <63002364+Smofe96@users.noreply.github.com>
2020-04-24 18:42:01 +0200

    Update

---
+++ GitUnityProjekt/Assets/AnimTeleporterSprite.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimTeleporterSprite : MonoBehaviour
+{
+    public ScoreManager MscoreManager;
+    public Animator transport;
+    // Start is called before the first frame update
+    void Start()
+    {
+       transport = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MscoreManager.score == 2)
+        {
+            transport.Play("AnimTeleporter");
+        }
+
+
+    }
+}
