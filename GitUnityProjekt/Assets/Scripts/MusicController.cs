afbd94c50287d7eca939d60dcde7fc4d0c2507be
Apoma96 <63002615+Apoma96@users.noreply.github.com>
2020-04-25 13:03:53 +0200

    Sound für Teleportaktivierung testweise hinzugefügt

---
+++ GitUnityProjekt/Assets/Scripts/MusicController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+
+    public static bool mcExists;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!mcExists)
+            mcExists = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
