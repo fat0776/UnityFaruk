2f08c6ac268920f2c8e318e6cddbb9faad6065c1
fat0776 <63000005+fat0776@users.noreply.github.com>
2020-04-26 18:55:05 +0200

    Sound Update + Teleporter an Sound angepasst

--- GitUnityProjekt/Assets/Scripts/PlayerScript.cs
+++ GitUnityProjekt/Assets/Scripts/PlayerScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public float moveSpeed = 5f;
+    public float moveSpeed = 7f;
     Vector2 movement;
     void Start()
     {
--- GitUnityProjekt/Assets/Scripts/TeleportScripts/Teleporter.cs
+++ GitUnityProjekt/Assets/Scripts/TeleportScripts/Teleporter.cs
@@ -1,23 +1,27 @@
 ﻿using System.Collections;
-
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Teleporter : MonoBehaviour { 
     public ScoreManager MyScoreManager;
-    
+     public AudioSource audioSource;
     public string levelName;
     void OnTriggerEnter2D(Collider2D other)
-
     {
-        //if (MyScoreManager.score ==2)
-        //{
+        if (MyScoreManager.score ==2)
+        {
             if (other.CompareTag("Player"))
             { 
-                SceneManager.LoadScene(levelName);
+                StartCoroutine(ExampleCoroutine()); //Untere Funktion wird aufgerufen -> wartet 0.5s damit Sound abgespielt wird und danach neue Szene laden
+                audioSource.Play();
             }
-       // }
+        }
+    }
 
+    IEnumerator ExampleCoroutine()
+    {
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene(levelName);
     }
 
 }
